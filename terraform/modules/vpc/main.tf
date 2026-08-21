95e0c2ea0c5f34d37ae32c3ae3080884fbff56ea
Nikhil Marathe <111993397+niks6996@users.noreply.github.com>
2026-08-21 18:31:28 +0100

    fix: correct NAT gateway formatting

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -80,7 +80,7 @@ resource "aws_nat_gateway" "this" {
   count = var.enable_nat_gateway ? 1 : 0
 
   allocation_id = aws_eip.nat[0].id
-  subnet_id      = aws_subnet.public[sort(keys(aws_subnet.public))[0]].id
+  subnet_id     = aws_subnet.public[sort(keys(aws_subnet.public))[0]].id
 
   tags = merge(var.tags, {
     Name = "${var.name_prefix}-nat"
