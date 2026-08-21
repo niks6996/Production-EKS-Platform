86e779b04fa1c4f7d77800d165fd185851d31545
Nikhil Marathe <111993397+niks6996@users.noreply.github.com>
2026-08-21 18:25:48 +0100

    fix: format VPC Terraform configuration

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -11,10 +11,10 @@ locals {
 module "vpc" {
   source = "../../modules/vpc"
 
-  name_prefix       = local.name_prefix
-  vpc_cidr          = var.vpc_cidr
-  public_subnets    = var.public_subnets
-  private_subnets   = var.private_subnets
+  name_prefix        = local.name_prefix
+  vpc_cidr           = var.vpc_cidr
+  public_subnets     = var.public_subnets
+  private_subnets    = var.private_subnets
   enable_nat_gateway = var.enable_nat_gateway
-  tags              = local.common_tags
+  tags               = local.common_tags
 }
